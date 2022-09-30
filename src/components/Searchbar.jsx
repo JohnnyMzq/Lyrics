34a64bce201851e05558db536ef9c0ea1f5a2da1
JohnnyMzq <89431959+JohnnyMzq@users.noreply.github.com>
2022-09-30 13:03:30 -0300

    Add files via upload

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import { FiSearch } from 'react-icons/fi';
+
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`);
+  };
+
+  return (
+    <form
+      autoComplete="off"
+      className="p-2 text-gray-400
+    focus-within:text-gray-600"
+      onSubmit={handleSubmit}
+    >
+      <label
+        htmlFor="search-field"
+        className="sr-only"
+      >
+        Search All Songs
+      </label>
+      <div className="flex fle-row justify-start items-center">
+        <FiSearch className="w-5 h-5 ml-4" />
+        <input
+          name="search-field"
+          autoComplete="off"
+          id="search-field"
+          placeholder="Search"
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="flax-1 bg-transparent border-none outline-none placeholder-gray-500 text-base p-4 text-white"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Searchbar;
